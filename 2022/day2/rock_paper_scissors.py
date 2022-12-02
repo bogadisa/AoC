f66d7333212143e2e8f7865fb334d0a9765104d2
Magnus Hoddevik <bjornhod@student.matnat.uio.no>
2022-12-02 23:00:10 +0100

    Refractor: day2 part 1

--- 2022/day2/rock_paper_scissors.py
+++ 2022/day2/rock_paper_scissors.py
@@ -4,28 +4,13 @@ choose = ["A", "B", "C", "A", "B", "C"]
 def simulate(opponent, you, part2=False):
     base_score = win_scores.index(you) + 1
     if not(part2):
-        if opponent == "A":
-            if you == "X":
-                return base_score + 3 
-            elif you == "Y":
-                return base_score + 6 
-            else:
-                return base_score + 0 
-        elif opponent == "B":
-            if you == "X":
-                return base_score + 0 
-            elif you == "Y":
-                return base_score + 3 
-            else:
-                return base_score + 6 
-        elif opponent == "C":
+        if choose[win_scores.index(you)] == choose[choose.index(opponent)]:
+            return 3 + base_score
+        elif choose[win_scores.index(you)] == choose[choose.index(opponent)+1]:
+            return 6 + base_score
+        else:
+            return base_score
             
-            if you == "X":
-                return base_score + 6 
-            elif you == "Y":
-                return base_score + 0 
-            else:
-                return base_score + 3 
     elif part2:
         if you == "X":
             return simulate(opponent, win_scores[choose.index(opponent)+2])
@@ -39,6 +24,7 @@ filename = "strat.txt"
 
 with open(filename, "r") as f:
     lines = "  ".join(f.readlines()).split("  ")
+    # print(lines)
     score1 = 0
     score2 = 0
     for line in lines:
