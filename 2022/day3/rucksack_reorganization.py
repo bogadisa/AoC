e22622f74289953d3920e15ddcbb79dbb5c0dff0
Magnus Hoddevik <bjornhod@student.matnat.uio.no>
2022-12-03 14:07:13 +0100

    Update: day2

--- 2022/day3/rucksack_reorganization.py
+++ 2022/day3/rucksack_reorganization.py
@@ -1,5 +1,4 @@
 import string
-import numpy as np
 
 filename = "inventory.txt"  
 
@@ -13,6 +12,6 @@ with open(filename, "r") as f:
         s1 += sum(rucksack)
         if i%3 == 0:
             group = [((string.ascii_lowercase.index(c.lower())+1) + c.isupper()*26)/line.count(c) for c in line.strip() if c in lines[i+1] and c in lines[i+2]]
-            s2 += np.sum(group)
+            s2 += sum(group)
     print(int(s1))
     print(int(s2))
