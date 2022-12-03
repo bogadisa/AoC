84b047ccc9d20af2aa13aa9b6b4df7e6345af2d1
Magnus Hoddevik <bjornhod@student.matnat.uio.no>
2022-12-03 14:29:43 +0100

    Update: day3

--- 2022/day3/rucksack_reorganization.py
+++ 2022/day3/rucksack_reorganization.py
@@ -8,10 +8,14 @@ with open(filename, "r") as f:
     lines = f.readlines()
     for i, line in enumerate(lines):
         size = len(line.strip())//2
-        rucksack = [(string.ascii_lowercase.index(c.lower())+1) + c.isupper()*26 for c in line[:size] if c in line[size:]]
+        rucksack = [(string.ascii_lowercase.index(c.lower())+1) + c.isupper()*26 
+                            for c in line[:size] 
+                            if c in line[size:]]
         s1 += rucksack[0]
         if i%3 == 0:
-            group = [(string.ascii_lowercase.index(c.lower())+1) + c.isupper()*26 for c in line.strip() if c in lines[i+1] and c in lines[i+2]]
+            group = [(string.ascii_lowercase.index(c.lower())+1) + c.isupper()*26 
+                            for c in line.strip()
+                            if c in lines[i+1] and c in lines[i+2]]
             s2 += group[0]
     print(int(s1))
     print(int(s2))
